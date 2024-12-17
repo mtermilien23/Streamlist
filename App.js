9f01c384b264971f8d0c3903d48e13fa1ff85c19
Michael Termilien <45954043+mtermilien23@users.noreply.github.com>
2024-12-16 22:26:09 -0500

    Create App.js

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './components/Header';
+import Navigation from './components/Navigation';
+import StreamList from './components/StreamList';
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <Navigation />
+            <StreamList />
+        </div>
+    );
+}
+
+export default App;

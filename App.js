d46795add244085a5db06870b565061b6a169a96
Michael Termilien <45954043+mtermilien23@users.noreply.github.com>
2025-01-13 19:00:41 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,16 +1,53 @@
-import React from 'react';
-import Header from './components/Header';
-import Navigation from './components/Navigation';
-import StreamList from './components/StreamList';
+import React, { useState, useEffect } from "react";
+import Navigation from "./components/Navigation";
+import Products from "./components/Products";
+import Cart from "./components/Cart";
+const App = () => {
+  const [cart, setCart] = useState(() => {
+    const savedCart = localStorage.getItem("cart");
+    return savedCart ? JSON.parse(savedCart) : [];
+  });
 
-function App() {
-    return (
-        <div>
-            <Header />
-            <Navigation />
-            <StreamList />
-        </div>
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart]);
+
+  const addToCart = (product) => {
+    const existingItem = cart.find((item) => item.id === product.id);
+    if (existingItem) {
+      setCart(
+        cart.map((item) =>
+          item.id === product.id ? { ...item, quantity: item.quantity + 1 } : item
+        )
+      );
+    } else {
+      setCart([...cart, { ...product, quantity: 1 }]);
+    }
+  };
+
+  const removeFromCart = (id) => {
+    setCart(cart.filter((item) => item.id !== id));
+  };
+
+  const updateQuantity = (id, quantity) => {
+    setCart(
+      cart.map((item) =>
+        item.id === id ? { ...item, quantity: Math.max(1, quantity) } : item
+      )
     );
-}
+  };
+
+  return (
+    <div>
+      <Navigation />
+      <Products addToCart={addToCart} />
+      <Cart
+        cart={cart}
+        removeFromCart={removeFromCart}
+        updateQuantity={updateQuantity}
+      />
+    </div>
+  );
+};
 
 export default App;

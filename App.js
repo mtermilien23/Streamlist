9cb22526fcd3ace7445911d6337b71d3eb778808
Michael Termilien <45954043+mtermilien23@users.noreply.github.com>
2025-01-20 15:29:34 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import React, { useState, useEffect } from "react";
+import Header from "./components/Header";
 import Navigation from "./components/Navigation";
 import Products from "./components/Products";
 import Cart from "./components/Cart";
+import MovieSearch from "./components/MovieSearch";
+import AddMovieForm from "./components/AddMovieForm"; // Import the AddMovieForm component
+import EventList from "./components/EventList";
+
 const App = () => {
   const [cart, setCart] = useState(() => {
     const savedCart = localStorage.getItem("cart");
     return savedCart ? JSON.parse(savedCart) : [];
   });
+
+  const [movies, setMovies] = useState([]); // State for movies
 
   useEffect(() => {
     localStorage.setItem("cart", JSON.stringify(cart));
@@ -17,7 +24,9 @@ const App = () => {
     if (existingItem) {
       setCart(
         cart.map((item) =>
-          item.id === product.id ? { ...item, quantity: item.quantity + 1 } : item
+          item.id === product.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
         )
       );
     } else {
@@ -37,9 +46,33 @@ const App = () => {
     );
   };
 
+  const handleAddMovie = (newMovie) => {
+    setMovies((prevMovies) => [...prevMovies, newMovie]);
+  };
+
+  const handleEdit = (id) => {
+    // Edit movie logic (placeholder)
+  };
+
+  const handleDelete = (id) => {
+    setMovies(movies.filter((movie) => movie.id !== id));
+  };
+
+  const handleToggleWatched = (id) => {
+    setMovies(
+      movies.map((movie) =>
+        movie.id === id ? { ...movie, watched: !movie.watched } : movie
+      )
+    );
+  };
+
   return (
     <div>
+      <Header />
       <Navigation />
+      <AddMovieForm onAddMovie={handleAddMovie} /> {/* Add movie form */}
+      <MovieSearch onAddMovie={handleAddMovie} /> {/* Pass add movie handler */}
+      <EventList />
       <Products addToCart={addToCart} />
       <Cart
         cart={cart}
